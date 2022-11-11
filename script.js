9c14fd7925b004a70b19785c3e14b76411964613
Guilherme Marcondes <54039442+marcondes-dev@users.noreply.github.com>
2022-11-10 21:38:05 -0300

    sexo11

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
 let vezJogador = true
-
+let jogada = 0
 function validarClick(event){
     var posicao = ('valor' + event)
     var valorPosicao =  document.getElementById(posicao).innerHTML  
     console.log(valorPosicao)
     if((valorPosicao != "X" && valorPosicao != "x") && (valorPosicao != "o" && valorPosicao != "O")){
 
-        var texto = ''
+        var texto = 'X'
+        var proximoJogador = 'X'
         if(vezJogador){
             texto = 'X'
+            proximoJogador = 'O'
         }
         else{
             texto = 'O'
+            proximoJogador = 'X'
         }
         document.getElementById(posicao).innerHTML = `${texto}`;
+        document.getElementById(posicao).style.color = 'black'
+        document.getElementById('vezJogador').innerHTML = `${'Vez do Jogador ' + proximoJogador}`;
         vezJogador = !vezJogador
         validarVitoria(posicao)
     }
@@ -21,5 +26,20 @@ function validarClick(event){
 }
 
 function validarVitoria(posicao){
-    var valorPosicao =  document.getElementById(posicao).innerHTML 
+    jogada += 1
+    var listaValores = new Array ()
+    
+    for(let i = 1; i < 10 ; i++){
+        var valor = 'valor' + i
+        console.log(valor)
+        var valorPosicao =  document.getElementById(valor).innerHTML 
+        if(jogada != 9){
+            break
+        }
+        listaValores.push(valorPosicao)
+    }
+    if(jogada = 9){
+
+    }
 }
+
