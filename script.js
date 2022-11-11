109d94fd65a60754d9931c42a864887e97c3bfac
Guilherme Marcondes <54039442+marcondes-dev@users.noreply.github.com>
2022-11-10 22:26:56 -0300

    sexo14 a ultima bimbada

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 let vezJogador = true
 let jogada = 0
+var proximoJogador = 'X'
+var vitoria = false
+
 function validarClick(event){
     var posicao = ('valor' + event)
     var valorPosicao =  document.getElementById(posicao).innerHTML  
-    console.log(valorPosicao)
-    if((valorPosicao != "X" && valorPosicao != "x") && (valorPosicao != "o" && valorPosicao != "O")){
+    if((valorPosicao != "X" && valorPosicao != "x") && (valorPosicao != "o" && valorPosicao != "O") && vitoria == false){
 
         var texto = 'X'
-        var proximoJogador = 'X'
         if(vezJogador){
             texto = 'X'
             proximoJogador = 'O'
@@ -20,26 +21,75 @@ function validarClick(event){
         document.getElementById(posicao).style.color = 'black'
         document.getElementById('vezJogador').innerHTML = `${'Vez do Jogador ' + proximoJogador}`;
         vezJogador = !vezJogador
-        validarVitoria(posicao)
+        validarPosicoes(posicao)
     }
 
 }
 
-function validarVitoria(posicao){
+function validarPosicoes(posicao){
     jogada += 1
     var listaValores = new Array ()
     
-    for(let i = 1; i < 10 ; i++){
-        var valor = 'valor' + i
-        console.log(valor)
+    for(let i = 0; i < 9 ; i++){
+        var valor = 'valor' + (i + 1)
         var valorPosicao =  document.getElementById(valor).innerHTML 
-        if(jogada != 9){
-            break
+        if(valorPosicao != 'A'){
+            listaValores[i]= valorPosicao
         }
-        listaValores.push(valorPosicao)
     }
-    if(jogada = 9){
-
-    }
+    validarVitoria(listaValores)
+    
 }
+
+function validarVitoria(listaValores){
+    console.log(listaValores)
+    if((listaValores[1]) && listaValores[0] == listaValores[1] && listaValores[1] == listaValores[2]){
+        ganhou()
+    }
+    else if((listaValores[4]) && listaValores[3] == listaValores[4] && listaValores[4] == listaValores[5]){
+        ganhou()
+    }
+    else if((listaValores[7]) && listaValores[6] == listaValores[7] && listaValores[7] == listaValores[8]){
+        ganhou()
+    }
+    else if((listaValores[3]) && listaValores[0] == listaValores[3] && listaValores[3] == listaValores[6]){
+        ganhou()
+    }
+    else if((listaValores[4]) && listaValores[1] == listaValores[4] && listaValores[4] == listaValores[7]){
+        ganhou()
+    }
+    else if((listaValores[5]) && listaValores[2] == listaValores[5] && listaValores[5] == listaValores[8]){
+        ganhou()
+    }
+    else if((listaValores[4]) && listaValores[0] == listaValores[4] && listaValores[4] == listaValores[8]){
+        ganhou()
+    }
+    else if((listaValores[4]) && listaValores[2] == listaValores[4] && listaValores[4] == listaValores[6]){
+        ganhou()
+    }
+    else if(jogada == 9 ){
+        velha()
+    }
+
+}
+
+function ganhou(){
+    vitoria = true
+    document.getElementById('botao2').style.display = 'block'
+    if(vezJogador){
+        document.getElementById('vezJogador').innerHTML = `${'O jogador O ganhou!'}`;
+    }else{
+        document.getElementById('vezJogador').innerHTML = `${'O jogador X ganhou!'}`;
+    }
+
+}
+
+function velha(){
+    vitoria = true
+    document.getElementById('botao2').style.display = 'block'
+    document.getElementById('vezJogador').innerHTML = `${'Deu velha!'}`;
+   
+
+}
+
 
