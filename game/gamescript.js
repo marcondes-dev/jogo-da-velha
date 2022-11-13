3491f553521ddb9c85cdf4b6724f6e5446da61e5
Matheus Romeike <matheuskauva2017@hotmail.com>
2022-11-13 14:20:34 -0300

    finalização

---
+++ game/gamescript.js
@@ -0,0 +1,99 @@
+let vezJogador = true
+let jogada = 0
+var proximoJogador = 'X'
+var fim = false
+
+function validarClick(event){
+    var posicao = ('valor' + event)
+    var valorPosicao =  document.getElementById(posicao).innerHTML  
+    if((valorPosicao != "X" && valorPosicao != "x") && (valorPosicao != "o" && valorPosicao != "O") && fim == false){
+
+        var texto = 'X'
+        if(vezJogador){
+            texto = 'X'
+            proximoJogador = 'O'
+        }
+        else{
+            texto = 'O'
+            proximoJogador = 'X'
+        }
+        document.getElementById(posicao).innerHTML = `${texto}`
+        document.getElementById(posicao).style.color = 'black'
+        document.getElementById('vezJogador').innerHTML = `${'Vez do Jogador ' + proximoJogador}`
+        vezJogador = !vezJogador
+        validarPosicoes(posicao)
+    }
+
+}
+
+function validarPosicoes(posicao){
+    jogada += 1
+    var listaValores = new Array ()
+    
+    for(let i = 0; i < 9 ; i++){
+        var valor = 'valor' + (i + 1)
+        var valorPosicao =  document.getElementById(valor).innerHTML 
+        if(valorPosicao != 'A'){
+            listaValores[i]= valorPosicao
+        }
+    }
+    validarVitoria(listaValores)
+    
+}
+
+function validarVitoria(listaValores){
+    if((listaValores[1]) && listaValores[0] == listaValores[1] && listaValores[1] == listaValores[2]){
+        ganhou()
+    }
+    else if((listaValores[4]) && listaValores[3] == listaValores[4] && listaValores[4] == listaValores[5]){
+        ganhou()
+    }
+    else if((listaValores[7]) && listaValores[6] == listaValores[7] && listaValores[7] == listaValores[8]){
+        ganhou()
+    }
+    else if((listaValores[3]) && listaValores[0] == listaValores[3] && listaValores[3] == listaValores[6]){
+        ganhou()
+    }
+    else if((listaValores[4]) && listaValores[1] == listaValores[4] && listaValores[4] == listaValores[7]){
+        ganhou()
+    }
+    else if((listaValores[5]) && listaValores[2] == listaValores[5] && listaValores[5] == listaValores[8]){
+        ganhou()
+    }
+    else if((listaValores[4]) && listaValores[0] == listaValores[4] && listaValores[4] == listaValores[8]){
+        ganhou()
+    }
+    else if((listaValores[4]) && listaValores[2] == listaValores[4] && listaValores[4] == listaValores[6]){
+        ganhou()
+    }
+    else if(jogada == 9 ){
+        velha()
+    }
+
+}
+
+function ganhou(){
+    fim = true
+
+    const audio = new Audio('../audio/vitoria.mp3')
+    audio.volume = 1
+    audio.play()
+
+    document.getElementById('botao2').style.display = 'block'
+    if(vezJogador){
+        document.getElementById('vezJogador').innerHTML = `${'O jogador O ganhou!'}`
+    }else{
+        document.getElementById('vezJogador').innerHTML = `${'O jogador X ganhou!'}`
+    }
+}
+
+function velha(){
+    fim = true
+
+    const audio = new Audio('../audio/velha.mp3')
+    audio.volume = 1
+    audio.play()
+
+    document.getElementById('botao2').style.display = 'block'
+    document.getElementById('vezJogador').innerHTML = `${'Deu velha!'}`
+}
